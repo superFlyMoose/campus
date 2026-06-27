7d8bcd0b01b1cd725ab0b25188c100bd2a2e5027
chens <3126674065@qq.com>
2026-06-27 11:32:44 +0800

    原版

--- src/main/java/com/campus/management/controller/HomeController.java
+++ src/main/java/com/campus/management/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.campus.management.controller;
 
+import com.campus.management.service.DashboardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final DashboardService dashboardService;
+
+    public HomeController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("systemName", "校园活动管理系统");
+        model.addAttribute("hotActivities", dashboardService.getHotActivities());
         return "index";
     }
 
